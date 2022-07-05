268b449bdc7b7ec7771b45d0a10f9c4b8c7d70d3
Nader Magdy <Nader.Magdy@integrant.com>
2022-07-05 20:25:39 +0200

    Add option to filter iteration capacity by project and team

--- src/iteration/iteration.controller.ts
+++ src/iteration/iteration.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { IAzureDevOpsConfig } from 'src/configs/azure-devops-config';
 import { AzureDevopsService } from 'src/services/azure-devops.service';
 
 @Controller('iteration')
 export class IterationController {
-    constructor(private readonly azureDevOpsService: AzureDevopsService) { }
+  constructor(private readonly azureDevOpsService: AzureDevopsService) { }
 
-  @Get("capacity")
-  async getIterations(filterPath : string = "") {
-    return this.azureDevOpsService.getCapacity(filterPath);
+  @Get('capacity/:project/:team')
+  async getIterations(@Param('project') project: string, @Param('team') team: string,@Query("f") filterPath : string = "") {
+    return this.azureDevOpsService.getCapacity(filterPath, <IAzureDevOpsConfig>{ project, team });
   }
 
   //teamdaysoff
--- src/services/azure-devops.service.ts
+++ src/services/azure-devops.service.ts
@@ -1,6 +1,7 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { lastValueFrom, map, Observable } from 'rxjs';
+import { IAzureDevOpsConfig } from 'src/configs/azure-devops-config';
 import { ICapacity } from 'src/models/capacity.model';
 import { IDaysOff } from 'src/models/days-off.model';
 import { IIteration } from 'src/models/iteration.model';
@@ -9,22 +10,22 @@ import { ConfigService } from './config.service';
 @Injectable()
 export class AzureDevopsService {
 
-    constructor(private readonly httpService: HttpService, 
-        private readonly configService : ConfigService) { }
+    constructor(private readonly httpService: HttpService,
+        private readonly configService: ConfigService) { }
 
-    async getCapacity(filterPath: string = "") :Promise<ICapacity[]> {
-        const currentIteration = await this.getCurrentIteration(filterPath);
-        return this.getCapacityPerIteration(currentIteration);
+    async getCapacity(filterPath: string = "", filter : IAzureDevOpsConfig = null): Promise<ICapacity[]> {
+        const currentIteration = await this.getCurrentIteration(filterPath, filter);
+        return this.getCapacityPerIteration(currentIteration, filter);
 
     }
 
-    private async getCapacityPerIteration(iteration: IIteration): Promise<ICapacity[]> {
-        const teamDaysOff = await this.getTeamDaysOff(iteration.id);
+    private async getCapacityPerIteration(iteration: IIteration, filter : IAzureDevOpsConfig = null): Promise<ICapacity[]> {
+        const teamDaysOff = await this.getTeamDaysOff(iteration.id, filter);
         let teamDaysOffCount = 0;
         teamDaysOff.forEach(daysOff => {
             teamDaysOffCount += this.getBusinessDatesCount(daysOff.start, daysOff.end);
         })
-        return lastValueFrom(this.httpService.get(this.buildBasicUrl(`teamsettings/iterations/${iteration.id}/capacities`))
+        return lastValueFrom(this.httpService.get(this.buildBasicUrl(`teamsettings/iterations/${iteration.id}/capacities`, filter))
             .pipe(map(res => res.data.value.map(c => {
                 let obj = <ICapacity>{
                     ...c.teamMember,
@@ -32,12 +33,12 @@ export class AzureDevopsService {
                     daysOff: teamDaysOffCount,
                     workingHours: 8,
                     workingDays: this.getBusinessDatesCount(iteration.startDate, iteration.finishDate),
-                    iterationName : iteration.name,
-                    iterationPath : iteration.path,
-                    iterationUrl : iteration.url,
-                    iterationStartDate : iteration.startDate,
-                    iterationFinishDate : iteration.finishDate,
-                    iterationTimeFrame : iteration.timeFrame
+                    iterationName: iteration.name,
+                    iterationPath: iteration.path,
+                    iterationUrl: iteration.url,
+                    iterationStartDate: iteration.startDate,
+                    iterationFinishDate: iteration.finishDate,
+                    iterationTimeFrame: iteration.timeFrame
                 };
                 c.daysOff.forEach(dayOff => {
                     obj.daysOff += this.getBusinessDatesCount(dayOff.start, dayOff.end);
@@ -47,10 +48,12 @@ export class AzureDevopsService {
             }))));
     }
 
-    getIterations(filterPath: string = ""): Observable<IIteration[]> {
-        return this.httpService.get(this.buildBasicUrl("teamsettings/iterations"))
+    getIterations(filterPath: string = "", filter : IAzureDevOpsConfig = null): Observable<IIteration[]> {
+        const team = filter?.team || this.configService.azureDevOpsConfigs.team;
+        return this.httpService.get(this.buildBasicUrl("teamsettings/iterations", filter))
             .pipe(
-                map(res => res.data.value.filter(x => x.path.indexOf(`${this.configService.azureDevOpsConfigs.team}\\${filterPath}`) > -1)
+                map(res => 
+                    res.data.value.filter(x => x.path.indexOf(`${team}\\${filterPath}`) > -1)
                     .map(iteration => (<IIteration>{
                         id: iteration.id,
                         name: iteration.name,
@@ -60,18 +63,20 @@ export class AzureDevopsService {
                     }))));
     }
 
-    async getTeamDaysOff(iterationId: string = null): Promise<IDaysOff[]> {
+    async getTeamDaysOff(iterationId: string = null, filter : IAzureDevOpsConfig = null): Promise<IDaysOff[]> {
         iterationId = iterationId ?? (await this.getCurrentIteration()).id;
-        return lastValueFrom(this.httpService.get(this.buildBasicUrl(`teamsettings/iterations/${iterationId}/teamdaysoff`))
+        return lastValueFrom(this.httpService.get(this.buildBasicUrl(`teamsettings/iterations/${iterationId}/teamdaysoff`, filter))
             .pipe(map(res => <IDaysOff[]>res.data.daysOff)));
     }
-    private async getCurrentIteration(filterPath: string = ""): Promise<IIteration> {
-        let iterations$ = this.getIterations(filterPath);
+    private async getCurrentIteration(filterPath: string = "", filter : IAzureDevOpsConfig = null): Promise<IIteration> {
+        let iterations$ = this.getIterations(filterPath, filter);
         let iterations = await lastValueFrom(iterations$);
         return iterations.filter(i => i.timeFrame == 'current')[0];
     }
-    private buildBasicUrl(endpoint: string): string {
-        return `https://${this.configService.azureDevOpsConfigs.token}@projects.integrant.com/${this.configService.azureDevOpsConfigs.organization}/${this.configService.azureDevOpsConfigs.project}/${this.configService.azureDevOpsConfigs.team}/_apis/work/${endpoint}`;
+    private buildBasicUrl(endpoint: string, filter : IAzureDevOpsConfig = null): string {
+        const project = filter?.project || this.configService.azureDevOpsConfigs.project;
+        const team = filter?.team || this.configService.azureDevOpsConfigs.team;
+        return `https://${this.configService.azureDevOpsConfigs.token}@projects.integrant.com/${this.configService.azureDevOpsConfigs.organization}/${project}/${team}/_apis/work/${endpoint}`;
     }
 
     private getBusinessDatesCount(startDate: Date, endDate: Date) {
